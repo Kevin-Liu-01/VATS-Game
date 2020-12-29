5fcc855ee2a94a89afccacf38655d2df2ef58ec7
Kevin  Liu <kk23907751@gmail.com>
2020-12-28 20:53:53 -0500

    Finished game.

--- src/components/metagross/Metagross.js
+++ src/components/metagross/Metagross.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import VATSON from "./VATS Sts/VATSON";
 import VATSOFF from "./VATS Sts/VATSOFF";
+import Practice from "./Practice";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -10,12 +11,13 @@ const Metagross = (props) => {
 
   const handleDirectoVATSClick = () => {
     setIsNotVats(!isNotVats);
-    toast.info(`V.A.T.S Switched. ${toastCounter}`);
-    setToastCounter(toastCounter + 1);
+    // toast.info(`V.A.T.S Switched. ${toastCounter}`);
+    // setToastCounter(toastCounter + 1);
   };
 
   return (
     <div>
+      <Practice />
       {isNotVats ? (
         <VATSOFF handleDirectoVATSClick={handleDirectoVATSClick} />
       ) : (
--- src/components/metagross/VATS Sts/VATSON.js
+++ src/components/metagross/VATS Sts/VATSON.js
@@ -5,30 +5,87 @@ import LEFTARM from "../VATS Pictures/METAGROSSLEFTVATS.gif";
 import HEAD from "../VATS Pictures/METAGROSSHEADVATS.gif";
 import RIGHTARM from "../VATS Pictures/METAGROSSRIGHTARM.gif";
 import BOTTOM from "../VATS Pictures/METAGROSSBOTTOMHEAD.gif";
-import Gun from "../VATS Pictures/Gunshooting.gif";
+import VATSNoned from "../VATS Pictures/MetagrossAttack3.gif";
+import WIN from "../VATS Pictures/WinMetagross2.gif";
 
 const VATSON = (props) => {
-  const [Health, setHealth] = useState(469);
+  const [Health, setHealth] = useState(6942);
   const [BodyPart, setBodyPart] = useState(0);
-
+  const [secondsLeft, setSecondsLeft] = useState(8);
   useEffect(() => {
-    console.log("bodypart is", BodyPart);
-  }, [BodyPart]);
-
+    if (secondsLeft > 0) {
+      const timerId = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1);
+      }, 750);
+      return () => clearTimeout(timerId);
+    }
+  });
   return (
     <div>
       {BodyPart === 5 ? (
-        ((<img src={Gun} style={styles.Gun} alt='VATSONE' />),
-        (<img src={BOTTOM} style={styles.VATSGREYStyle} alt='VATSONE' />))
+        <>
+          <img src={BOTTOM} style={styles.VATSGREYStyle} alt='VATSONE' />
+          <button
+            style={styles.DamageStyle}
+            onClick={() => {
+              setHealth(Health - (Math.floor(60 * Math.random()) + 40));
+            }}
+          >
+            FIRE
+          </button>
+          <button style={styles.TimerStyle}>
+            Time before attack: {secondsLeft}
+          </button>
+        </>
       ) : BodyPart === 1 ? (
-        <img src={HEAD} style={styles.VATSGREYStyle} alt='VATSONE' />
+        <>
+          <img src={HEAD} style={styles.VATSGREYStyle} alt='VATSONE' />
+          <button
+            style={styles.DamageStyle}
+            onClick={() => {
+              setHealth(Health - (Math.floor(48 * Math.random()) + 40));
+            }}
+          >
+            FIRE
+          </button>
+          <button style={styles.TimerStyle}>
+            Time before attack: {secondsLeft}
+          </button>
+        </>
       ) : BodyPart === 2 ? (
-        <img src={LEFTARM} style={styles.VATSGREYStyle} alt='VATSONE' />
+        <>
+          <img src={LEFTARM} style={styles.VATSGREYStyle} alt='VATSONE' />
+          <button
+            style={styles.DamageStyle}
+            onClick={() => {
+              setHealth(Health - (Math.floor(32 * Math.random()) + 40));
+            }}
+          >
+            FIRE
+          </button>
+          <button style={styles.TimerStyle}>
+            Time before attack: {secondsLeft}
+          </button>
+        </>
       ) : BodyPart === 4 ? (
-        <img src={RIGHTARM} style={styles.VATSGREYStyle} alt='VATSONE' />
+        <>
+          <img src={RIGHTARM} style={styles.VATSGREYStyle} alt='VATSONE' />
+          <button
+            style={styles.DamageStyle}
+            onClick={() => {
+              setHealth(Health - (Math.floor(29 * Math.random()) + 40));
+            }}
+          >
+            FIRE
+          </button>
+          <button style={styles.TimerStyle}>
+            Time before attack: {secondsLeft}
+          </button>
+        </>
       ) : (
         <img src={VATSONImage} style={styles.StartingStyle} alt='VATSONE' />
       )}
+
       <button style={styles.HealthStyle}>Health: {Health}</button>
       <button
         style={styles.ButtonStyle}
@@ -36,6 +93,9 @@ const VATSON = (props) => {
         className='UndoVATS'
       >
         TERMINATE V.A.T.S
+      </button>
+      <button style={styles.TimerStyle}>
+        Time before attack: {secondsLeft}
       </button>
       <div>
         <div>
@@ -45,14 +105,12 @@ const VATSON = (props) => {
                 setBodyPart(0);
               } else if (BodyPart !== 1) {
                 setBodyPart(1);
-                setHealth(Health - 53);
               }
             }}
             style={styles.VATSButtonStyle}
           >
             95%
           </button>
-
           <button style={styles.HealthStyle}>Health: {Health}</button>
           <button
             style={styles.ButtonStyle}
@@ -62,73 +120,110 @@ const VATSON = (props) => {
             TERMINATE V.A.T.S
           </button>
         </div>
-        <button
-          onClick={() => {
-            if (BodyPart === 2) {
-              setBodyPart(0);
-            } else if (BodyPart !== 2) {
-              setBodyPart(2);
-              setHealth(Health - 28);
-            }
-          }}
-          style={styles.ButtonStyle3}
-        >
-          72%
-        </button>
-
-        <button style={styles.HealthStyle}>Health: {Health}</button>
-        <button
-          style={styles.ButtonStyle}
-          onClick={props.handleDirectoVATSClick}
-          className='UndoVATS'
-        >
-          TERMINATE V.A.T.S
-        </button>
-        <button
-          onClick={() => {
-            if (BodyPart === 4) {
-              setBodyPart(0);
-            } else if (BodyPart !== 4) {
-              setBodyPart(4);
-              setHealth(Health - 23);
-            }
-          }}
-          style={styles.ButtonStyle4}
-        >
-          65%
-        </button>
-
-        <button style={styles.HealthStyle}>Health: {Health}</button>
-        <button
-          style={styles.ButtonStyle}
-          onClick={props.handleDirectoVATSClick}
-          className='UndoVATS'
-        >
-          TERMINATE V.A.T.S
-        </button>
-        <button
-          onClick={() => {
-            if (BodyPart === 5) {
-              setBodyPart(0);
-            } else if (BodyPart !== 5) {
-              setBodyPart(5);
-              setHealth(Health - 41);
-            }
-          }}
-          style={styles.ButtonStyle5}
-        >
-          23%
-        </button>
-
-        <button style={styles.HealthStyle}>Health: {Health}</button>
-        <button
-          style={styles.ButtonStyle}
-          onClick={props.handleDirectoVATSClick}
-          className='UndoVATS'
-        >
-          TERMINATE V.A.T.S
-        </button>
+        <div>
+          <button
+            onClick={() => {
+              if (BodyPart === 2) {
+                setBodyPart(0);
+              } else if (BodyPart !== 2) {
+                setBodyPart(2);
+              }
+            }}
+            style={styles.ButtonStyle3}
+          >
+            72%
+          </button>
+          <button style={styles.HealthStyle}>Health: {Health}</button>
+          <button
+            style={styles.ButtonStyle}
+            onClick={props.handleDirectoVATSClick}
+            className='UndoVATS'
+          >
+            TERMINATE V.A.T.S
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              if (BodyPart === 4) {
+                setBodyPart(0);
+              } else if (BodyPart !== 4) {
+                setBodyPart(4);
+              }
+            }}
+            style={styles.ButtonStyle4}
+          >
+            65%
+          </button>
+          <button style={styles.HealthStyle}>Health: {Health}</button>
+          <button
+            style={styles.ButtonStyle}
+            onClick={props.handleDirectoVATSClick}
+            className='UndoVATS'
+          >
+            TERMINATE V.A.T.S
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              if (BodyPart === 5) {
+                setBodyPart(0);
+              } else if (BodyPart !== 5) {
+                setBodyPart(5);
+              }
+            }}
+            style={styles.ButtonStyle5}
+          >
+            23%
+          </button>
+          <button style={styles.HealthStyle}>Health: {Health}</button>
+          <button
+            style={styles.ButtonStyle}
+            onClick={props.handleDirectoVATSClick}
+            className='UndoVATS'
+          >
+            TERMINATE V.A.T.S
+          </button>
+          {secondsLeft === 0 && Health > 0 ? (
+            <>
+              <img src={VATSNoned} style={styles.VATSGREYStyle} alt='VATSONE' />
+              <button
+                style={styles.LoseStyle}
+                onClick={props.handleDirectoVATSClick}
+                className='UndoVATS'
+              >
+                REPLAY
+              </button>
+            </>
+          ) : null}
+          {secondsLeft === 0 && Health <= 0 ? (
+            <>
+              <img src={WIN} style={styles.VATSGREYStyle} alt='VATSONE' />
+              <button
+                style={styles.LoseStyle}
+                onClick={props.handleDirectoVATSClick}
+                className='UndoVATS'
+              >
+                REPLAY
+              </button>
+            </>
+          ) : null}
+          {Health < 0 ? (
+            <>
+              <img src={WIN} style={styles.VATSGREYStyle} alt='VATSONE' />
+              <button
+                style={styles.LoseStyle}
+                onClick={props.handleDirectoVATSClick}
+                className='UndoVATS'
+              >
+                REPLAY
+              </button>
+            </>
+          ) : null}
+        </div>
       </div>
+      {Health < 0 ? <>setHealth(0)</> : null}
     </div>
   );
 };
